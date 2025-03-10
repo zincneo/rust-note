fabe3ae5a09d5c10961070ad9ac66ce5bc1d400a
zincneo <zincneoestonia@gmail.com>
2025-03-10 08:27:12 +0000

    feat(ch10): ch10-03
    * 结构体中使用生命周期

--- src/ch10/mod.rs
+++ src/ch10/mod.rs
@@ -20,6 +20,9 @@ fn _ch10_01_concept_lifetime() {
 /// # 函数中使用生命周期
 /// - 以'开头在泛型参数列表中来定义生命周期标识符
 /// - 生命周期标识符并非凭空变出来生命周期而是为了将生命周期往更短的变量约束以通过编译
+/// - 函数的返回值如果是一个引用类型，那么其生命周期不可能无中生有，只会有两种来源
+///   1. 跟随某一个实参
+///   2. 跟随函数体内新建引用的生命周期 -> 这种时候必然是报错，因为试图返回一个临时变量的引用，要用到这种需求就直接返回对应数据的所有权就可以了
 fn _ch10_02_function_lifetime() {
     // 定义一个函数用来返回两个字符串切片中更长的一个
     // 这里就涉及到一个生命周期的问题，因为切片是一个引用，编译器不知道返回变量谁会活的更久
@@ -41,6 +44,29 @@ fn _ch10_02_function_lifetime() {
     }
 }
 
+/// ## 结构体中使用生命周期
+/// - 在泛型参数列表中加上了生命周期标识符
+/// - 在引用&符号后使用生命周期标识符
+/// - 该字段的生命周期会跟随被赋予的引用
+/// - 被引用的数据生命周期要大于等于该结构体的生命周期
+fn _ch10_03_struct_lifetime() {
+    #[derive(Debug)]
+    struct ImportantExcerpt<'a> {
+        part: &'a str,
+    }
+    let mut i;
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    {
+        let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+        i = ImportantExcerpt {
+            part: first_sentence,
+        };
+        i.part = &novel;
+    }
+    // 下一行可以通过编译，因为i在离开代码块之前引用了生命周期更长的novel变量
+    println!("{:?}", i);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,5 +79,10 @@ mod tests {
     #[test]
     fn ch10_02() {
         assert_eq!(_ch10_02_function_lifetime(), ());
+    }
+
+    #[test]
+    fn ch10_03() {
+        assert_eq!(_ch10_03_struct_lifetime(), ());
     }
 }
