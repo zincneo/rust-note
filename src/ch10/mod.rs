0442a0180fa1e9b2a0f2365ca4eeeb5083383875
zincneo <zincneoestonia@gmail.com>
2025-03-10 08:13:45 +0000

    feat(ch10): ch10-02
    * 函数签名中使用生命周期标识符

--- src/ch10/mod.rs
+++ src/ch10/mod.rs
@@ -17,6 +17,30 @@ fn _ch10_01_concept_lifetime() {
     }
 }
 
+/// # 函数中使用生命周期
+/// - 以'开头在泛型参数列表中来定义生命周期标识符
+/// - 生命周期标识符并非凭空变出来生命周期而是为了将生命周期往更短的变量约束以通过编译
+fn _ch10_02_function_lifetime() {
+    // 定义一个函数用来返回两个字符串切片中更长的一个
+    // 这里就涉及到一个生命周期的问题，因为切片是一个引用，编译器不知道返回变量谁会活的更久
+    // 因此为了通过生命周期标识符可以约束在不清楚生命周期谁最长的时候使用时对齐到短的那一个
+    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
+    let x = "hello";
+    let rstr;
+    {
+        let y = "world!";
+        rstr = longest(x, y);
+        println!("{rstr}");
+        // 生命周期标识符将rstr的生命周期约束为和y相同，在离开代码块之后不能再被使用
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,5 +48,10 @@ mod tests {
     #[test]
     fn ch10_01() {
         assert_eq!(_ch10_01_concept_lifetime(), ());
+    }
+
+    #[test]
+    fn ch10_02() {
+        assert_eq!(_ch10_02_function_lifetime(), ());
     }
 }
