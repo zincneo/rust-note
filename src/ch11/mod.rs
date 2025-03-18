fe1af1c772535ea84f24d07cd9162920e6285e64
zincneo <zincneoestonia@gmail.com>
2025-03-18 08:14:08 +0000

    feat(ch11): ch11-03
    * 实现Display和Debug特征

--- src/ch11/mod.rs
+++ src/ch11/mod.rs
@@ -17,7 +17,9 @@ fn _ch11_01_output_macro() {
 /// - rust在格式化输出中提供了{}和{:?}作为占位符
 /// - {}适用于std::fmt::Display特征的数据类型
 /// - {:?}适用于std::fmt::Debug特征的数据类型
+/// - {:#?}也适用于实现std::fmt::Debug特征的数据类型，更适合调试
 /// - 实际上大多数标准库中提供的类型都实现了Debug特征
+/// - 要在格式化输出的时候输出{}本身要使用{{}}
 fn _ch11_02_placeholder() {
     #[derive(Debug)]
     struct Person {
@@ -34,6 +36,29 @@ fn _ch11_02_placeholder() {
     println!("{:?}, {:?}, {:?}, {:?}", i, s, v, p);
 }
 
+/// ## 实现Display和Debug特征的方式
+/// - Debug特征特供了derive宏为自定义类型自动实现
+/// - 由于孤儿规则，如果要为标准库中没有实现Display的类型实现Display则需要使用自建新类型包裹
+/// - Display和Debug都需要实现一个fmt方法，该方法返回一个std::fmt::Result类型的值
+/// - 可以使用write!宏返回std::fmt::Result类型的值
+fn _ch11_03_impl_Dispaly_Debug() {
+    use std::fmt::{Debug, Display};
+    struct Position(i32, i32);
+    impl Display for Position {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{} {}", self.0, self.1)
+        }
+    }
+    impl Debug for Position {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Position {{ x: {}, y: {} }}", self.0, self.1)
+        }
+    }
+    let pos = Position(0, 0);
+    println!("{pos}");
+    println!("{:?}", pos);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +71,10 @@ mod tests {
     #[test]
     fn ch11_02() {
         assert_eq!(_ch11_02_placeholder(), ());
+    }
+
+    #[test]
+    fn ch11_03() {
+        assert_eq!(_ch11_03_impl_Dispaly_Debug(), ());
     }
 }
