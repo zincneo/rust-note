2f5a143179c9edbb6ad9576e330729a6008b4227
zincneo <zincneoestonia@gmail.com>
2025-04-09 14:19:45 +0800

    feat(language): ch16-03
    * Drop特征
    * 手动回收内存的函数std::mem::drop
    * Drop使用场景

--- src/language/ch16/mod.rs
+++ src/language/ch16/mod.rs
@@ -4,6 +4,8 @@
 1. [Box](./fn.f01_box.html)
 
 2. [Deref](./ch16_02_deref/index.html)
+
+3. [Drop](./fn.f03_drop.html)
 */
 
 /**
@@ -55,6 +57,108 @@ pub fn f01_box() {
 
 pub mod ch16_02_deref;
 
+/**
+# Drop
+
+- Rust中没有GC，通过所有权机制由编译器在编译期就决定变量应该在何时回收
+- 可以为类型实现Drop特征，当变量在释放的时候会执行Drop特征中定义的drop方法进行资源回收
+    - 变量级别，按照逆序的方式，后定义先先执行drop方法
+    - 结构体内部，按照顺序的方式，先定义的字段先执行drop方法
+```rust
+struct HasDrop1;
+struct HasDrop2;
+impl Drop for HasDrop1 {
+    fn drop(&mut self) {
+        println!("Dropping HasDrop1!");
+    }
+}
+impl Drop for HasDrop2 {
+    fn drop(&mut self) {
+        println!("Dropping HasDrop2!");
+    }
+}
+struct HasTwoDrops {
+    one: HasDrop1,
+    two: HasDrop2,
+}
+impl Drop for HasTwoDrops {
+    fn drop(&mut self) {
+        println!("Dropping HasTwoDrops!");
+    }
+}
+
+struct Foo;
+
+impl Drop for Foo {
+    fn drop(&mut self) {
+        println!("Dropping Foo!")
+    }
+}
+{
+    let _x = HasTwoDrops {
+        two: HasDrop2,
+        one: HasDrop1,
+    };
+    let _foo = Foo;
+}
+// Dropping Foo // 后定义的变量先执行drop方法
+// Dropping HasTwoDrop // 嵌套的结构体先执行外部的drop方法
+// Dropping HasDrop1 // 结构体内部按照定义的顺序，更早定义的字段先执行drop方法
+// Dropping HasDrop2
+```
+- 禁止手动调用Drop特征中的drop方法，如果需要提前回收要使用std::mem::drop
+```rust
+let a = Foo;
+std::mem::drop(a);
+```
+- 大部分情况下不需要自定义Drop特征，常见的使用场景
+    - 文件描述符、网络socket
+    - 一些特殊的数据结构，如没法形成尾递归优化且回收会导致爆栈的结构(例如链表)
+*/
+#[allow(dead_code)]
+pub fn f03_drop() {
+    struct HasDrop1;
+    struct HasDrop2;
+    impl Drop for HasDrop1 {
+        fn drop(&mut self) {
+            println!("Dropping HasDrop1!");
+        }
+    }
+    impl Drop for HasDrop2 {
+        fn drop(&mut self) {
+            println!("Dropping HasDrop2!");
+        }
+    }
+    struct HasTwoDrops {
+        one: HasDrop1,
+        two: HasDrop2,
+    }
+    impl Drop for HasTwoDrops {
+        fn drop(&mut self) {
+            println!("Dropping HasTwoDrops!");
+        }
+    }
+
+    struct Foo;
+
+    impl Drop for Foo {
+        fn drop(&mut self) {
+            println!("Dropping Foo!")
+        }
+    }
+
+    let _x = HasTwoDrops {
+        two: HasDrop2,
+        one: HasDrop1,
+    };
+    let _foo = Foo;
+    println!("Running!");
+    {
+        let a = Foo;
+        std::mem::drop(a);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -69,5 +173,10 @@ mod tests {
         assert_eq!(ch16_02_deref::f02_01_star_mark(), ());
         assert_eq!(ch16_02_deref::f02_02_auto_deref(), ());
         assert_eq!(ch16_02_deref::f02_03_deref_impl(), ());
+    }
+
+    #[test]
+    fn ch16_03() {
+        assert_eq!(f03_drop(), ());
     }
 }
