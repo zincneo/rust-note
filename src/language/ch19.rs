25770ac43f6513c6121d89b318fd574570645063
zincneo <zincneoestonia@gmail.com>
2025-04-23 11:42:57 +0800

    feat(language): ch19-02 ch19-03
    * 文件读取
    * 文件写入

--- src/language/ch19.rs
+++ src/language/ch19.rs
@@ -2,9 +2,11 @@
 # Rust读写文件
 
 1. [打开/创建文件](./fn.f01_open_create.html)
-2. 读取文件
-3. 写入文件
+2. [读取文件](./fn.f02_read.html)
+3. [写入文件](./fn.f03_write.html)
 */
+
+use std::io::{Read, Write};
 
 /**
 # 打开/创建文件
@@ -92,6 +94,81 @@ pub fn f01_open_create() {
         .open(path);
 }
 
+/**
+# 读取文件
+1. std::fs::read方法直接以字节方式读取路径内容，返回一个`Result<Vec[u8], _>`
+    ```rust
+    let _data = std::fs::read("data");
+    ```
+2. 对于打开的文件File类型实例进行读取
+    - read_to_string将整个文件读入为字符串
+    - read_to_end将整个读入为二进制动态数组
+    ```rust
+    if let Ok(mut file) = std::fs::File::open("data") {
+        // 读入为字符串
+        let mut data = String::new();
+        if let Ok(_) = file.read_to_string(&mut data) {
+            println!("{:#?}", data);
+        }
+    }
+    if let Ok(mut file) = std::fs::File::open("data") {
+        // 读入为二进制数组
+        let mut data = Vec::new();
+        if let Ok(_) = file.read_to_end(&mut data) {
+            println!("{:#?}", data);
+        }
+    }
+    ```
+*/
+pub fn f02_read() {
+    let _data = std::fs::read("data");
+    if let Ok(mut file) = std::fs::File::open("data") {
+        // 读入为字符串
+        let mut data = String::new();
+        if let Ok(_) = file.read_to_string(&mut data) {
+            println!("{:#?}", data);
+        }
+    }
+    if let Ok(mut file) = std::fs::File::open("data") {
+        // 读入为二进制数组
+        let mut data = Vec::new();
+        if let Ok(_) = file.read_to_end(&mut data) {
+            println!("{:#?}", data);
+        }
+    }
+}
+
+/**
+# 写入文件
+1. std::fs::write方法直接写入字符串到文件中
+    ```rust
+    match std::fs::write("data", "test") {
+        Ok(_) => (),
+        Err(e) => println!("{:#?}", e),
+    }
+    ```
+2. 写入或者追加写入需要通过打开方式控制
+    ```rust
+    let Ok(mut file) = std::fs::OpenOptions::new().append(true).open("data") else {
+        println!("打开失败");
+        return;
+    };
+    file.write("abc\n".to_string().as_bytes()).unwrap();
+    ```
+*/
+pub fn f03_write() {
+    match std::fs::write("data", "test\n") {
+        Ok(_) => (),
+        Err(e) => println!("{:#?}", e),
+    }
+    let Ok(mut file) = std::fs::OpenOptions::new().append(true).open("data") else {
+        println!("打开失败");
+        return;
+    };
+    file.write("abc\n".to_string().as_bytes()).unwrap();
+    file.write_all("abcdef\n".to_string().as_bytes()).unwrap();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -99,5 +176,15 @@ mod tests {
     #[test]
     fn ch19_01() {
         assert_eq!(f01_open_create(), ());
+    }
+
+    #[test]
+    fn ch19_02() {
+        assert_eq!(f02_read(), ());
+    }
+
+    #[test]
+    fn ch19_03() {
+        assert_eq!(f03_write(), ());
     }
 }
