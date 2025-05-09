b22c17c21c2b944b610b11e4cdd4d0e60eb917d6
zincneo <zincneoestonia@gmail.com>
2025-05-09 11:35:24 +0800

    feat(language): ch21-03
    * try_join和select

--- src/language/ch21.rs
+++ src/language/ch21.rs
@@ -17,6 +17,7 @@
 
 ## [1. async/await](./fn.f01_async_await.html)
 ## [2. 同时启动多个异步任务](./fn.f02_join.html)
+## [3. try_join和select](./fn.f03_try_join_select.html)
 */
 
 /**
@@ -100,6 +101,98 @@ pub fn f02_join() {
     futures::executor::block_on(async_main());
 }
 
+/**
+# try_join和select
+- join!宏会等待所有异步任务结束之后才结束处理结果
+- try_join宏在任意一个异步任务报错之后就结束处理结果
+    ```rust
+    async fn f1() -> Result<(), String> {
+        println!("任务一");
+        Ok(())
+    }
+    async fn f2() -> Result<(), String> {
+        println!("任务二");
+        Err("任务2报错".to_string())
+    }
+    async fn f3() -> Result<(), String> {
+        println!("任务三");
+        Err("任务3报错".to_string())
+    }
+    async fn async_main_1() {
+        // try_join将会在第一个报错的Future之后结束所有Future
+        // 返回值类型是Result<T, E>，try_join要求所有的Future的E类型相同，T则是一个元组，包裹所有的Future的T类型
+        let res = try_join!(f1(), f2(), f3());
+        match res {
+            Ok(((), (), ())) => (),
+            Err(e) => println!("{}", e),
+        }
+        // join则会执行完所有的Future
+        let (_res_1, _res_2, _res_3) = join!(f1(), f2(), f3());
+    }
+    ```
+- select!宏在在每一个异步任务结束之后就直接处理结果
+    ```rust
+        use futures::{
+            future::FutureExt, // for `.fuse()`
+            pin_mut,
+            select,
+        };
+        let t1 = f1().fuse();
+        let t2 = f2().fuse();
+        let t3 = f3().fuse();
+        // 并发执行，结束一个全部结束
+        pin_mut!(t1, t2, t3);
+        select! {
+            _ = t1 => println!("任务1执行结束"),
+            _ = t2 => println!("任务2执行结束"),
+            _ = t3 => println!("任务3执行结束"),
+        }
+    ```
+
+*/
+pub fn f03_try_join_select() {
+    use futures::{executor::block_on, join, try_join};
+    async fn f1() -> Result<(), String> {
+        println!("任务一");
+        Ok(())
+    }
+    async fn f2() -> Result<(), String> {
+        println!("任务二");
+        Err("任务2报错".to_string())
+    }
+    async fn f3() -> Result<(), String> {
+        println!("任务三");
+        Err("任务3报错".to_string())
+    }
+    async fn async_main_1() {
+        // try_join将会在第一个报错的Future之后结束所有Future
+        // 返回值类型是Result<T, E>，try_join要求所有的Future的E类型相同，T则是一个元组，包裹所有的Future的T类型
+        let res = try_join!(f1(), f2(), f3());
+        match res {
+            Ok(((), (), ())) => (),
+            Err(e) => println!("{}", e),
+        }
+        // join则会执行完所有的Future
+        let (_res_1, _res_2, _res_3) = join!(f1(), f2(), f3());
+        use futures::{
+            future::FutureExt, // for `.fuse()`
+            pin_mut,
+            select,
+        };
+        let t1 = f1().fuse();
+        let t2 = f2().fuse();
+        let t3 = f3().fuse();
+        // 并发执行，结束一个全部结束
+        pin_mut!(t1, t2, t3);
+        select! {
+            _ = t1 => println!("任务1执行结束"),
+            _ = t2 => println!("任务2执行结束"),
+            _ = t3 => println!("任务3执行结束"),
+        }
+    }
+    block_on(async_main_1());
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -112,5 +205,10 @@ mod tests {
     #[test]
     fn ch21_02() {
         assert_eq!(f02_join(), ());
+    }
+
+    #[test]
+    fn ch21_03() {
+        assert_eq!(f03_try_join_select(), ());
     }
 }
