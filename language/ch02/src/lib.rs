7e0b91905b535b3c072deae04e8d9c343162dd7d
zincneo <zincneoestonia@gmail.com>
2025-06-23 09:03:33 +0800

    feat(language): ch02
    * 所有权原则
    * 引用和借用以及可变引用

--- language/ch02/src/lib.rs
+++ language/ch02/src/lib.rs
@@ -1,4 +1,19 @@
+mod m01_ownership;
+mod m02_reference;
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test01_ownership() {
+        assert_eq!(m01_ownership::f01_ownership(), ());
+        assert_eq!(m01_ownership::f02_data_bind(), ());
+    }
+
+    #[test]
+    fn test02_reference() {
+        assert_eq!(m02_reference::f01_ref(), ());
+        assert_eq!(m02_reference::f02_mut_ref(), ());
+    }
 }
