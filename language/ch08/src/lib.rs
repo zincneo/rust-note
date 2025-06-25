1524b0f1b12b02f01c54a2f0e72a373678eb7018
zincneo <zincneoestonia@gmail.com>
2025-06-25 17:04:22 +0800

    feat(language): ch08
    * 模式使用场景
    * 解构

--- language/ch08/src/lib.rs
+++ language/ch08/src/lib.rs
@@ -1,5 +1,7 @@
 mod m01_match;
 mod m02_pattern;
+mod m03_scence;
+mod m04_deconstruction;
 
 #[cfg(test)]
 mod tests {
@@ -20,5 +22,21 @@ mod tests {
         assert_eq!(m02_pattern::f03_let_pattern(), ());
         assert_eq!(m02_pattern::f04_argument_pattern(), ());
         assert_eq!(m02_pattern::f05_let_else_pattern(), ());
+    }
+
+    #[test]
+    fn test03_scence() {
+        assert_eq!(m03_scence::f01_range_pattern(), ());
+        assert_eq!(m03_scence::f02_composition_pattern(), ());
+        assert_eq!(m03_scence::f03_if_guard_pattern(), ());
+        assert_eq!(m03_scence::f04_at_pattern(), ());
+    }
+
+    #[test]
+    fn test04_deconstruction() {
+        assert_eq!(m04_deconstruction::f01_tuple(), ());
+        assert_eq!(m04_deconstruction::f02_struct(), ());
+        assert_eq!(m04_deconstruction::f03_array(), ());
+        assert_eq!(m04_deconstruction::f04_enum(), ());
     }
 }
