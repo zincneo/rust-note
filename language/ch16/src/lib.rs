ca94be1e5c564ae84a58809318622150ee8ae02c
zincneo <zincneoestonia@gmail.com>
2025-07-02 10:33:30 +0800

    feat(language): ch16
    * 标准库线程模型
    * 标准库线程消息通道channel

--- language/ch16/src/lib.rs
+++ language/ch16/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod m01_thread;
+mod m02_mpsc;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test01_thread() {
+        assert_eq!(m01_thread::f01_thread(), ());
+        assert_eq!(m01_thread::f02_barrier(), ());
+    }
+
+    #[test]
+    fn test02_mpsc() {
+        assert_eq!(m02_mpsc::f01_async_mpsc(), ());
+        assert_eq!(m02_mpsc::f02_try_recv(), ());
+        assert_eq!(m02_mpsc::f03_sync_channel(), ());
     }
 }
---
+++ language/ch16/src/m01_thread.rs
@@ -0,0 +1,62 @@
+#![allow(dead_code)]
+#![allow(unused)]
+
+/**
+# 线程模型
+- Rust标准库`std::thread`提供和os线程一比一的线程模型
+- 常用方法
+    - spawn创建一个新的线程
+        - 参数是一个闭包，线程执行传入的闭包
+        - 不会阻塞当前的线程
+        - 返回一个JoinHandle类型的变量
+        - 返回值执行join方法可以阻塞当前线程直到子线程运行结束
+    - sleep方法，让当前线程休眠指定时间
+- Rust中main线程结束的时候就结束所有线程
+- 父线程非main线程的线程在父线程结束的时候不会结束
+*/
+pub fn f01_thread() {
+    use std::thread::{sleep, spawn};
+    use std::time::Duration;
+
+    let t1 = spawn(|| {
+        for i in 0..3 {
+            sleep(Duration::from_millis(1000));
+            println!("first thread: {i}");
+        }
+    });
+
+    let t2 = spawn(|| {
+        for i in 0..3 {
+            sleep(Duration::from_millis(1000));
+            println!("second thread: {i}");
+        }
+    });
+
+    t1.join();
+    t2.join();
+}
+
+/**
+# 线程屏障
+- 线程安全的引用计数std::sync::Arc
+- std::sync::Barrier提供让多个线程执行到某个点之后一起执行的能力
+- Barrier::new传入线程的数量，当调用Barrier::wait方法的线程数量达到new中传入的数量之后继续执行
+*/
+pub fn f02_barrier() {
+    use std::sync::{Arc, Barrier};
+    let mut handles = Vec::with_capacity(6);
+    let barrier = Arc::new(Barrier::new(6));
+
+    for i in 0..6 {
+        let b = barrier.clone();
+        handles.push(std::thread::spawn(move || {
+            println!("before {i} wait");
+            b.wait();
+            println!("after {i} wait, strong count {}", Arc::strong_count(&b));
+        }));
+    }
+
+    for handle in handles {
+        handle.join();
+    }
+}
