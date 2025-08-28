6481af05027e38966dec24680b5843906e976936
zincneo <zincneoestonia@gmail.com>
2025-08-28 11:59:59 +0800

    feat(bevy_ecs): bevy_model
    * 添加UI状态处理

--- 06-bevy_ecs/bevy_model/src/lib.rs
+++ 06-bevy_ecs/bevy_model/src/lib.rs
@@ -1,19 +1,24 @@
 mod event;
 mod system;
+mod ui_store;
 use arc_swap::ArcSwapOption;
 use bevy_ecs::{schedule::Schedule, world::World};
 use crossbeam::channel::{Receiver, Sender, unbounded};
+use dashmap::DashMap;
 use event::*;
 use std::{
+    ops::DerefMut,
     sync::{Arc, LazyLock},
     thread::{JoinHandle, spawn},
 };
 use system::*;
+use ui_store::*;
 
 pub struct Model {
     sender: ArcSwapOption<Sender<Box<dyn ModelEvent>>>,
     handle: ArcSwapOption<JoinHandle<()>>,
     system_queue: SystemQueue,
+    ui_store: UIStore,
 }
 
 impl Model {
@@ -22,6 +27,7 @@ impl Model {
             sender: ArcSwapOption::from(None),
             handle: ArcSwapOption::from(None),
             system_queue: SystemQueue::get_queue(),
+            ui_store: DashMap::with_shard_amount(32),
         }
     }
 
@@ -38,6 +44,7 @@ impl Model {
 
     pub fn deinit(&self) -> Option<()> {
         self.system_queue.clear();
+        self.ui_store.clear();
         self.sender.swap(None)?.send(BasicEvent::stop()).ok()?;
         let handle = self.handle.swap(None)?;
         match std::sync::Arc::try_unwrap(handle) {
@@ -72,6 +79,42 @@ impl Model {
         T: Fn(&mut Schedule) + Sync + Send + 'static,
     {
         MODEL.system_queue.push(Box::new(system));
+    }
+
+    pub fn add_ui_instance<T>(instance: T) -> UIInstanceId
+    where
+        T: UITrait,
+    {
+        let instance_id = UIInstanceId::new_instance::<T>();
+        MODEL.ui_store.insert(instance_id, UIState::new(instance));
+        instance_id
+    }
+
+    pub fn remove_ui_instance(instance_id: UIInstanceId) {
+        UIInstanceId::destory_instance(instance_id);
+        MODEL.ui_store.remove(&instance_id);
+    }
+
+    pub fn get_ui_instance<T>(instance_id: UIInstanceId) -> Option<T>
+    where
+        T: UITrait,
+    {
+        MODEL
+            .ui_store
+            .get_mut(&instance_id)?
+            .deref_mut()
+            .take_new::<T>()
+    }
+
+    pub fn set_instance<T>(instance: T, instance_id: UIInstanceId) -> Option<()>
+    where
+        T: UITrait,
+    {
+        MODEL
+            .ui_store
+            .get_mut(&instance_id)?
+            .deref_mut()
+            .set_new(instance)
     }
 }
 
---
+++ 06-bevy_ecs/bevy_model/src/ui_store.rs
@@ -0,0 +1,99 @@
+use dashmap::{DashMap, DashSet};
+use std::{
+    any::{Any, TypeId},
+    ops::DerefMut,
+    sync::LazyLock,
+};
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub struct UIInstanceId(TypeId, u16);
+
+pub trait UITrait: Any + PartialEq + Clone + Send + Sync + 'static {}
+impl<T: Any + PartialEq + Clone + Send + Sync + 'static> UITrait for T {}
+
+impl UIInstanceId {
+    pub(crate) fn new_instance<T>() -> UIInstanceId
+    where
+        T: Any,
+    {
+        let type_id = TypeId::of::<T>();
+        let instance_id;
+        let Some(mut count) = TYPE_INSTANCE_MAP.get_mut(&type_id) else {
+            instance_id = UIInstanceId(type_id, 0);
+            INSTANCE_SET.insert(instance_id);
+            return instance_id;
+        };
+        let count = count.deref_mut();
+        while let Some(_) = INSTANCE_SET.get(&UIInstanceId(type_id, *count)) {
+            *count = count.wrapping_add(1);
+        }
+        instance_id = UIInstanceId(type_id, *count);
+        INSTANCE_SET.insert(instance_id);
+        TYPE_INSTANCE_MAP.insert(type_id, *count);
+        instance_id
+    }
+    pub(crate) fn destory_instance(instancd_id: UIInstanceId) -> Option<UIInstanceId> {
+        INSTANCE_SET.remove(&instancd_id)
+    }
+}
+
+pub(crate) static TYPE_INSTANCE_MAP: LazyLock<DashMap<TypeId, u16>> = LazyLock::new(DashMap::new);
+pub(crate) static INSTANCE_SET: LazyLock<DashSet<UIInstanceId>> = LazyLock::new(DashSet::new);
+
+pub(crate) struct UIState {
+    new: Option<Box<dyn Any + Send + Sync + 'static>>,
+    old: Option<Box<dyn Any + Send + Sync + 'static>>,
+}
+
+impl Default for UIState {
+    fn default() -> Self {
+        Self {
+            new: None,
+            old: None,
+        }
+    }
+}
+
+impl UIState {
+    pub(crate) fn new<T>(instance: T) -> Self
+    where
+        T: UITrait,
+    {
+        let old_instance = instance.clone();
+        UIState {
+            new: Some(Box::new(instance)),
+            old: Some(Box::new(old_instance)),
+        }
+    }
+
+    pub(crate) fn take_new<T>(&mut self) -> Option<T>
+    where
+        T: UITrait,
+    {
+        let old = *self.new.take()?.downcast::<T>().ok()?;
+        self.old = Some(Box::new(old.clone()));
+        Some(old)
+    }
+
+    pub(crate) fn set_new<T>(&mut self, instance: T) -> Option<()>
+    where
+        T: UITrait,
+    {
+        let old = self.take_new::<T>()?;
+        if instance != old {
+            let instance = instance.clone();
+            self.new = Some(Box::new(instance));
+        }
+        self.set_old(instance);
+        Some(())
+    }
+
+    fn set_old<T>(&mut self, instance: T)
+    where
+        T: UITrait,
+    {
+        self.old = Some(Box::new(instance));
+    }
+}
+
+pub(crate) type UIStore = DashMap<UIInstanceId, UIState>;
